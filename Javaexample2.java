1d5981976937bac00e51999173511518fc042ae9
sandesh <19l31a05k2@vignaniit.edu.in>
2023-11-20 12:33:34 +0530

    adding a new file

---
+++ Javaexample2.java
@@ -0,0 +1,16 @@
+public class Javaexample2 {
+public static void main(String args[]){
+    int num1 = 10, num2;
+    num2 = num1;
+    System.out.println("=output: "+num2);
+    num2 += num1;
+    System.out.println("+=output:"+num2);
+    num2-=num1;
+    System.out.println("-=output:"+num2);
+    num2*=num1;
+    System.out.println("*=output"+num2);
+    num2/=num1;
+    System.out.println("/=output"+num2);
+
+}
+}
